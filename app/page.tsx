ca1349d8676251fefeb183115fe26792ea3bb988
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-06-30 11:36:23 -0700

    added some logos

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import Image from "next/image";
 import React from "react";
+import nextLogo from "../public/next.svg";
+import vercelLogo from "../public/vercel.svg";
+import polygonLogo from "../public/polygon-io.svg";
 import getData from "./fetch";
 import { useState, useEffect } from "react";
 import { Center, Box } from "@chakra-ui/react";
@@ -27,9 +30,23 @@ function App() {
 
         <Box
           id="footer"
-          className="p-4 left-0 bottom-0 w-screen h-1/6 bg-purple-200 fixed"
+          className="p-4 left-0 bottom-0 w-screen h-12 bg-purple-200 fixed inline-flex"
         >
-          footer
+          <Image
+            src={nextLogo}
+            alt="Next.js Logo"
+            className="max-h-full w-1/12"
+          />
+          <Image
+            src={vercelLogo}
+            alt="Vercel Logo"
+            className="max-h-full w-1/12"
+          />
+          <Image
+            src={polygonLogo}
+            alt="Polygon.io Logo"
+            className="max-h-full w-1/12"
+          />
         </Box>
       </Center>
     </>
