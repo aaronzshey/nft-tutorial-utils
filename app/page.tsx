786f50e4953389e7777354183cf35f35fb045016
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-06-29 23:09:05 -0700

    started styling with tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,34 @@ import Image from "next/image";
 import React from "react";
 import getData from "./fetch";
 import { useState, useEffect } from "react";
+import { Center, Box } from "@chakra-ui/react";
 
 function App() {
   const [data, setData] = useState("");
+
   const showData = async () => {
     const data = await getData();
-    setData(data);
+    console.log(data.userId);
+    const userId = data.userId;
+    setData(userId);
   };
 
   useEffect(() => {
     showData();
   }, []);
 
-  //convert the data into
-
   return (
     <>
-      <div>cog is good</div>
-      <div>{JSON.stringify(data, null, 2)}</div>
+      <Center className="h-screen w-screen bg-blue-100" id="wrapper">
+        <Center className="border-t-2 h-1/2 w-1/2 bg-violet-300">{data}</Center>
+
+        <Box
+          id="footer"
+          className="p-4 left-0 bottom-0 w-screen h-1/6 bg-purple-200 fixed"
+        >
+          footer
+        </Box>
+      </Center>
     </>
   );
 }
