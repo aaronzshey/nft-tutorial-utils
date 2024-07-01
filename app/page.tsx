6350425111005f807716321ea3e86ebfa077d4d7
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-06-30 20:21:58 -0700

    continued styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import polygonLogo from "../public/polygon-io.svg";
 import alchemyLogo from "../public/alchemy.svg";
 import getData from "./fetch";
 import { useState, useEffect } from "react";
-import { Center, Box } from "@chakra-ui/react";
+import { Center, Box, HStack, StackDivider } from "@chakra-ui/react";
 
 import { polygonResponseType, polygonData } from "./polygonType";
 
@@ -27,33 +27,38 @@ function App() {
 
   return (
     <>
-      <Center className="h-screen w-screen bg-blue-100" id="wrapper">
-        <Center className="border-t-2 h-1/2 w-1/2 bg-violet-300">{data}</Center>
+      <Center className="h-screen w-screen" id="wrapper">
+        <Center className="h-1/2 w-1/2 bg-slate-300">{data}</Center>
 
         <Box
-          id="footer"
-          className="p-4 left-0 bottom-0 w-screen h-12 bg-purple-200 fixed inline-flex"
+          id="footer-parent"
+          className="left-0 bottom-0 w-screen h-12 fixed bg-gradient-to-r from-black via-white to-white pt-0.5"
         >
-          <Image
-            src={nextLogo}
-            alt="Next.js Logo"
-            className="max-h-full w-1/12"
-          />
-          <Image
-            src={vercelLogo}
-            alt="Vercel Logo"
-            className="max-h-full w-1/12"
-          />
-          <Image
-            src={polygonLogo}
-            alt="Polygon.io Logo"
-            className="max-h-full w-1/12"
-          />
-          <Image
-            src={alchemyLogo}
-            alt="Alchemy Logo"
-            className="max-h-full w-1/12"
-          />
+          <HStack
+            className="w-screen inline-flex inherit bg-white"
+            spacing="10px"
+          >
+            <Image
+              src={nextLogo}
+              alt="Next.js Logo"
+              className="h-auto  w-1/12 pl-1"
+            />
+            <Image
+              src={vercelLogo}
+              alt="Vercel Logo"
+              className="h-auto w-1/12 p-4"
+            />
+            <Image
+              src={polygonLogo}
+              alt="Polygon.io Logo"
+              className="h-auto w-1/12 p-4"
+            />
+            <Image
+              src={alchemyLogo}
+              alt="Alchemy Logo"
+              className="max-h-full w-1/12 p-4"
+            />
+          </HStack>
         </Box>
       </Center>
     </>
---
+++ app/serveractionsTest.tsx
@@ -0,0 +1,3 @@
+export default function actionTest(formData: FormData) {
+  console.log(formData.entries);
+}
