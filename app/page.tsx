db310d642cdc6e5bb8a1700aea8324c0e5166ee0
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-07-02 00:06:44 -0700

    added icons, better fetching, and more styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,88 +1,116 @@
 "use client";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 import React from "react";
+import { Suspense } from "react";
 import nextLogo from "../public/next.svg";
 import vercelLogo from "../public/vercel.svg";
 import polygonLogo from "../public/polygon-io.svg";
 import alchemyLogo from "../public/alchemy.svg";
+import ethereumLogo from "../public/ethereum.svg";
 import getData from "./fetch";
 import { useState, useEffect } from "react";
-import { Center, Box, HStack, VStack } from "@chakra-ui/react";
+import { Center, Box, HStack, VStack, Text } from "@chakra-ui/react";
 
-import { polygonResponseType, polygonData } from "./polygonType";
+import { polygonResponseType } from "./polygonType";
 
 function App() {
-  const [data, setData] = useState(0);
+  const [polygonResponse, setPolygonResponse] = useState(
+    {} as polygonResponseType,
+  );
 
-  const showData = async () => {
-    const data: polygonResponseType = await getData();
-    console.log(data.results);
-    const results = data.results[0].c;
-    setData(results);
+  let ethValueRaw = useSearchParams().get("ethValue");
+  let ethValue = 0.001;
+  if (ethValueRaw) {
+    ethValue = parseFloat(ethValueRaw);
+  }
+
+  const getPolygonResponse = async () => {
+    const polygonResponse: polygonResponseType = await getData();
+    setPolygonResponse(polygonResponse);
   };
 
   useEffect(() => {
-    showData();
+    getPolygonResponse();
   }, []);
 
+  let ethInUSD: number;
+
+  if (!polygonResponse.results) {
+    ethInUSD = 0;
+  } else {
+    ethInUSD = polygonResponse.results[0].c;
+  }
   return (
-    <>
-      <Center className="h-screen w-screen" id="wrapper">
-        <Center className="h-1/2 w-1/2 bg-slate-300">{data}</Center>
+    <Suspense>
+      <>
+        <Center className="h-screen w-screen" id="wrapper">
+          <Center className="h-screen w-screen font-black text-6xl bg-radial-gradient">
+            <Suspense>
+              <Text className="inherit">{ethValue}</Text>
+            </Suspense>
+            <Image
+              src={ethereumLogo}
+              alt="Ethereum Logo"
+              className="h-1/12 w-1/12 p-4"
+            />
+            = ${(ethInUSD * ethValue).toFixed(3)}
+          </Center>
 
-        <VStack
-          id="footer-parent"
-          spacing={0}
-          align="stretch"
-          className="z-0 left-0 bottom-0 w-screen h-1/12 absolute"
-        >
-          <Box
-            id="animated-ribbon"
-            className="h-px transition-all duration-1000"
-            background="linear-gradient(to right, black, black, black, white, white)"
-            backgroundSize="200% 200%"
-            backgroundPosition="100% 100%"
-          ></Box>
-
-          <HStack
-            className="z-10 left-0 bottom-0 w-screen "
-            spacing="10px"
-            id="footer"
-            onMouseEnter={() => {
-              document.getElementById(
-                "animated-ribbon",
-              ).style.backgroundPosition = "0% 0%";
-            }}
-            onMouseLeave={() => {
-              document.getElementById(
-                "animated-ribbon",
-              ).style.backgroundPosition = "100% 100%";
-            }}
+          <VStack
+            id="footer-parent"
+            spacing={0}
+            align="stretch"
+            className="z-0 left-0 bottom-0 w-screen h-1/12 absolute"
           >
-            <Image
-              src={nextLogo}
-              alt="Next.js Logo"
-              className="h-auto  w-1/12 pl-1"
-            />
-            <Image
-              src={vercelLogo}
-              alt="Vercel Logo"
-              className="h-auto w-1/12 p-4"
-            />
-            <Image
-              src={polygonLogo}
-              alt="Polygon.io Logo"
-              className="h-auto w-1/12 p-4"
-            />
-            <Image
-              src={alchemyLogo}
-              alt="Alchemy Logo"
-              className="max-h-full w-1/12 p-4"
-            />
-          </HStack>
-        </VStack>
-      </Center>
-    </>
+            <Box
+              id="animated-ribbon"
+              className="h-px transition-all duration-1000"
+              background="linear-gradient(to right, black, black, black, white, white)"
+              backgroundSize="200% 200%"
+              backgroundPosition="100% 100%"
+            ></Box>
+
+            <HStack
+              className="z-10 left-0 bottom-0 w-screen "
+              spacing="10px"
+              id="footer"
+              onMouseEnter={() => {
+                document.getElementById(
+                  "animated-ribbon",
+                ).style.backgroundPosition = "0% 0%";
+              }}
+              onMouseLeave={() => {
+                document.getElementById(
+                  "animated-ribbon",
+                ).style.backgroundPosition = "100% 100%";
+              }}
+            >
+              <Image
+                src={nextLogo}
+                alt="Next.js Logo"
+                className="h-auto  w-1/12 pl-1"
+              />
+              <Image
+                src={vercelLogo}
+                alt="Vercel Logo"
+                className="h-auto w-1/12 p-4"
+              />
+              <Image
+                src={polygonLogo}
+                alt="Polygon.io Logo"
+                className="h-auto w-1/12 p-4"
+              />
+              <Image
+                src={alchemyLogo}
+                alt="Alchemy Logo"
+                className="max-h-full w-1/12 p-4"
+              />
+            </HStack>
+          </VStack>
+        </Center>
+      </>
+    </Suspense>
   );
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,10 @@ const config: Config = {
       theme: {
         "200%": "200%",
       },
+      backgroundImage: {
+        "radial-gradient":
+          "radial-gradient(circle at top left, mediumslateblue 0%, transparent 50%), radial-gradient(circle at 100% 50%, lightcyan 0%, transparent 50%), radial-gradient(circle at 0% 100%, mistyrose 0%, transparent 75%)",
+      },
     },
   },
   content: [
