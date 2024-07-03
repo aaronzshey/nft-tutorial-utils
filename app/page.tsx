669043c4cb8a1a70770a75d869ccd76da5b3774c
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-07-03 00:58:38 -0700

    done with conversion calculator

--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,19 @@
 import React from "react";
 
 //library imports
-import { useSearchParams } from "next/navigation";
-import { ChangeEvent } from "react";
 import { useState, useEffect } from "react";
 
 //local imports
-import Image from "next/image";
-import ethereumLogo from "../public/ethereum.svg";
 import { polygonResponseType } from "./polygonType";
 import fetchPolygonResponse from "./fetchPolygonResponse";
 
 //component imports
-import {
-  Center,
-  Text,
-  FormControl,
-  Input,
-  VStack,
-  HStack,
-} from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
 import Footer from "../components/Footer";
-
 import GetEthValueFromParam from "../components/GetEthValueFromParam";
 
 function App() {
   const [ethConversionRate, setEthConversionRate] = useState(0);
-  const [marketCloseTime, setMarketCloseTime] = useState(new Date(0));
-  const [ethValue, setEthValue] = useState(0.001);
 
   /* these states are necessary because the polygonResponse is not immediately available.
   This stage of validation is required to avoid messy-looking errors or undefined errors
@@ -46,34 +32,9 @@ function App() {
     }
   };
 
-  const getMarketCloseTime = async () => {
-    const marketCloseTimeRaw: polygonResponseType =
-      await fetchPolygonResponse();
-
-    if (!marketCloseTimeRaw.results) {
-      setMarketCloseTime(new Date(0));
-    } else {
-      setMarketCloseTime(new Date(marketCloseTimeRaw.results[0].t));
-    }
-  };
-
   useEffect(() => {
     getEthConversionRate();
-    getMarketCloseTime();
   }, []);
-
-  //https://stackoverflow.com/a/72415437/12981681
-  function handleForm(e: ChangeEvent<HTMLInputElement>) {
-    e.preventDefault();
-    const ethFormValue = e.currentTarget.value;
-    console.log(e.currentTarget.value);
-
-    !isNaN(+ethFormValue) ? setEthValue(+ethFormValue) : setEthValue(0);
-  }
-
-  function copy(currentLink) {
-    navigator.clipboard.writeText(currentLink);
-  }
 
   return (
     <>
@@ -81,48 +42,8 @@ function App() {
         className="h-screen w-screen font-black text-6xl bg-radial-gradient"
         id="wrapper"
       >
-        {/* this vstack makes it easy to put subtext under the main data area */}
-        <VStack>
-          {/* this hstack contains the main data area */}
-          <HStack>
-            <FormControl>
-              <Input
-                className="focus: outline-none text-right w-44 overflow-x-scroll border-b-2 border-black bg-transparent caret-color: currentColor;"
-                defaultValue={ethValue}
-                onChange={handleForm}
-              />
-            </FormControl>
-            <Image
-              src={ethereumLogo}
-              alt="Ethereum Logo"
-              className="p-2"
-              width="100"
-              height="100"
-            />
-
-            <GetEthValueFromParam
-              ethValueInput={ethValue}
-              ethConversionRate={ethConversionRate}
-            />
-          </HStack>
-
-          <Text className="font-light text-sm">
-            Conversion rate from closing price on{" "}
-            {marketCloseTime.toLocaleDateString()},{" "}
-            {marketCloseTime.toLocaleTimeString()},{" "}
-            {marketCloseTime.toString().match(/\(.*\)/g)}.
-          </Text>
-          <Text
-            className="font-light text-xs cursor-copy hover:underline"
-            onClick={() =>
-              copy("eth-tutorial-utils.vercel.app/?ethValue=" + ethValue)
-            }
-          >
-            Click here to copy a share link to the current value.
-          </Text>
-        </VStack>
+        <GetEthValueFromParam ethConversionRate={ethConversionRate} />
       </Center>
-
       <Footer />
     </>
   );
--- components/GetEthValueFromParam.tsx
+++ components/GetEthValueFromParam.tsx
@@ -1,30 +1,110 @@
 "use client";
 import React from "react";
-import { useState, useEffect, Suspense } from "react";
+import { useState, useEffect, ChangeEvent, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
-import { Text } from "@chakra-ui/react";
+import { Text, HStack, VStack, FormControl, Input } from "@chakra-ui/react";
 
-export default function GetEthValueFromParam({
-  ethValueInput,
-  ethConversionRate,
-}) {
+import Image from "next/image";
+import ethereumLogo from "../public/ethereum.svg";
+import { polygonResponseType } from "../app/polygonType";
+
+import fetchPolygonResponse from "../app/fetchPolygonResponse";
+
+/*
+
+Ultimately, I had to move most of my helper functions into this file, due to this feature of Next:
+https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
+
+Since I wanted this app to be able to copy a link to the current form value, along with updating the form's value
+based on the URL, I had to move every function involving the form or the URL into this file - which turns out to be
+pretty much all of them.  While I tried to modularize my code and avoid spaghetti code, I still ended up with this
+monstrosity of a file.  Maybe in the future Next will remove this rather odd limitation, but for now,
+this is what I have.
+
+*/
+
+export default function GetEthValueFromParam({ ethConversionRate }) {
   const [ethValue, setEthValue] = useState(0.001);
-
   const ethValueRawFromParams = useSearchParams().get("ethValue");
-
   //this function will only run once, on load.  if no params are present, default to 0.001
   useEffect(() => {
     if (ethValueRawFromParams) {
       setEthValue(+ethValueRawFromParams);
-    } else {
-      setEthValue(ethValueInput);
     }
-  }, [ethValueRawFromParams, ethValueInput]);
+
+    getMarketCloseTime();
+  }, [ethValueRawFromParams]);
+
+  /*
+  While marketCloseTime doesn't need any ethValues, I did want it to be above the copy link to url text.
+  So, it had to go in this file.  It's a bit of a mess, but it works.
+  */
+  const [marketCloseTime, setMarketCloseTime] = useState(new Date(0));
+  const getMarketCloseTime = async () => {
+    const marketCloseTimeRaw: polygonResponseType =
+      await fetchPolygonResponse();
+
+    if (!marketCloseTimeRaw.results) {
+      setMarketCloseTime(new Date(0));
+    } else {
+      setMarketCloseTime(new Date(marketCloseTimeRaw.results[0].t));
+    }
+  };
+
+  //https://stackoverflow.com/a/72415437/12981681
+  function handleForm(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    const ethFormValue = e.currentTarget.value;
+    console.log(e.currentTarget.value);
+
+    !isNaN(+ethFormValue) ? setEthValue(+ethFormValue) : setEthValue(0);
+  }
+
+  function copy(currentLink: string) {
+    navigator.clipboard.writeText(currentLink);
+  }
 
   return (
     <Suspense>
-      <Text>= ${(ethConversionRate * ethValue).toFixed(3)}</Text>
+      {/* this vstack makes it easy to put subtext under the main data area */}
+
+      <VStack>
+        {/* this hstack contains the main data area */}
+
+        <HStack>
+          <FormControl>
+            <Input
+              className="focus: outline-none text-right w-44 overflow-x-scroll border-b-2 border-black bg-transparent caret-color: currentColor;"
+              defaultValue={ethValue}
+              onChange={handleForm}
+            />
+          </FormControl>
+          <Image
+            src={ethereumLogo}
+            alt="Ethereum Logo"
+            className="p-2"
+            width="100"
+            height="100"
+          />
+
+          <Text>= ${(ethConversionRate * ethValue).toFixed(3)}</Text>
+        </HStack>
+        <Text className="font-light text-sm">
+          Conversion rate from closing price on{" "}
+          {marketCloseTime.toLocaleDateString()},{" "}
+          {marketCloseTime.toLocaleTimeString()},{" "}
+          {marketCloseTime.toString().match(/\(.*\)/g)}.
+        </Text>
+        <Text
+          className="font-light text-xs cursor-copy hover:underline"
+          onClick={() =>
+            copy("eth-tutorial-utils.vercel.app/?ethValue=" + ethValue)
+          }
+        >
+          Click here to copy a share link to the current value.
+        </Text>
+      </VStack>
     </Suspense>
   );
 }
